1c132e0c8fb9870d1d16b7fd255ef7d12a18a59c
Ondřej Běhávka <obehavka@student.42heilbronn.de>
2024-12-11 13:19:41 +0100

    multi threading

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <pthread.h>
 
 typedef char i8;
 typedef unsigned char u8;
@@ -14,6 +15,8 @@ typedef unsigned long u64;
 #define PRINT_ERROR(cstring) write(STDERR_FILENO, cstring, sizeof(cstring) - 1)
 
 #pragma pack(1)
+
+#define NUM_THREADS 8
 
 typedef struct bmp_header
 {
@@ -45,6 +48,16 @@ typedef struct s_position{
 	u32 col;
 } t_position;
 
+typedef struct {
+    t_content* content;
+    t_header* header;
+    u32 start_row;
+    u32 end_row;
+    t_position result;
+    int found;
+} 	t_thread_data;
+
+
 struct file_content   read_entire_file(char* filename)
 {
 	char* file_data = 0;
@@ -61,16 +74,14 @@ struct file_content   read_entire_file(char* filename)
 	return (struct file_content){file_data, file_size};
 }
 
-int is_possible_header(u32 row, u32 col, t_header* header)
+inline static int is_possible_header(u32 row, u32 col, t_header* header)
 {
 	if (row + 7 < header->height && col + 7 < header->width)
-	{
 		return 1;
-	}
 	return 0;
 }
 
-i8 *position_to_pointer(t_content* content, t_position* position)
+inline static i8 *position_to_pointer(t_content* content, t_position* position)
 {
 	t_header *header = (t_header*)content->data;
 	return content->data + header->data_offset +
@@ -103,29 +114,55 @@ int confirm_header( t_content* content, i8* data_address)
 	return 1;
 }
 
-t_position traverse_file(t_content* content)
-{
-	t_header *header = (t_header*)content->data;
-	i8* data_address;
-	u8 channel_R, channel_G, channel_B;
+void* search_header(void* arg) {
+    t_thread_data* data = (t_thread_data*)arg;
+    t_content* content = data->content;
+    t_header* header = data->header;
+    i8* data_address;
+    u8 channel_R, channel_G, channel_B;
 
-	for (u32 row = 0; row < header->height; row++)
-	{
-		for (u32 col = 0; col < header->width; col++)
-		{
-			data_address = position_to_pointer(content, &(t_position){row, col});
-			channel_B = *data_address;
-			channel_G = *(data_address + 1);
-			channel_R = *(data_address + 2);
-			//printf("B: %u, G: %u, R: %u\n", channel_B, channel_G, channel_R);
-			if (channel_B == 127 && channel_G == 188 && channel_R == 217 && is_possible_header(row, col, header))
-				if(confirm_header(content, data_address))
-				{
-					return (t_position){row, col};
-				}
-		}
-	}
-	return (t_position){header->height, header->width};
+    for (u32 row = data->start_row; row < data->end_row; row++) {
+        for (u32 col = 0; col < header->width; col++) {
+            data_address = position_to_pointer(content, &(t_position){row, col});
+            channel_B = *data_address;
+            channel_G = *(data_address + 1);
+            channel_R = *(data_address + 2);
+            if (channel_B == 127 && channel_G == 188 && channel_R == 217 && is_possible_header(row, col, header)) {
+                if (confirm_header(content, data_address)) {
+                    data->result = (t_position){row, col};
+                    data->found = 1;
+                    return NULL;
+                }
+            }
+        }
+    }
+    data->found = 0;
+    return NULL;
+}
+
+t_position traverse_file(t_content* content) {
+    t_header* header = (t_header*)content->data;
+    pthread_t threads[NUM_THREADS];
+    t_thread_data thread_data[NUM_THREADS];
+    u32 rows_per_thread = header->height / NUM_THREADS;
+    t_position result = {header->height, header->width};
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].content = content;
+        thread_data[i].header = header;
+        thread_data[i].start_row = i * rows_per_thread;
+        thread_data[i].end_row = (i == NUM_THREADS - 1) ? header->height : (i + 1) * rows_per_thread;
+        pthread_create(&threads[i], NULL, search_header, &thread_data[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+        if (thread_data[i].found) {
+            result = thread_data[i].result;
+            break;
+        }
+    }
+    return result;
 }
 
 u32	get_msg_len(t_content* content, t_position* position)
@@ -157,14 +194,13 @@ void read_message(t_content* content, t_position* position, u16 msg_len)
 			where_am_i++;
 		}
 	}
-	write(STDOUT_FILENO, "\n", 1);
 }
 
 
 int main(int argc, char** argv)
 {
-	t_position position;
-	u32 msg_len;
+	t_position	position;
+	u32			msg_len;
 
 	if (argc != 2)
 	{
@@ -177,12 +213,8 @@ int main(int argc, char** argv)
 		PRINT_ERROR("Failed to read file\n");
 		return 1;
 	}
-	struct bmp_header* header = (struct bmp_header*) file_content.data;
 	position = traverse_file(&file_content);
 	msg_len = get_msg_len(&file_content, &position);
-	printf("Position: %u, %u\n", position.row, position.col);
 	read_message(&file_content, &position, msg_len);
-	printf("Message length: %u\n", msg_len);
-	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
 	return 0;
 }
